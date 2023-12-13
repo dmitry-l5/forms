9d36918ce9401e20acb931bd1618611236c1637e
Gogs <gogs@fake.local>
2023-12-13 12:00:35 +0900

    @

--- resources/views/components/result_base.blade.php
+++ resources/views/components/result_base.blade.php
@@ -1,5 +1,5 @@
 <div class="border border-2 border-sky-700 bg-white my-4 py-2 pt-0">
-    <div class=" bg-sky-700 text-start px-4 border-b border-neutral-300 text-white " title="{{ $description ?? '' }}">
+    <div class=" bg-sky-100 text-start px-4 border-b border-neutral-300 text-black font-semibold " title="{{ $description ?? '' }}">
         {{ $title }}
     </div>
     <div class="">
